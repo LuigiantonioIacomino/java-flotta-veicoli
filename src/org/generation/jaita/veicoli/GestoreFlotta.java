542786c761333b114c86f60f5e2e9617874b3324
LuigiantonioIacomino <iacominoluigi54@gmail.com>
2023-12-14 18:16:47 +0100

    fourth commit

--- src/org/generation/jaita/veicoli/GestoreFlotta.java
+++ src/org/generation/jaita/veicoli/GestoreFlotta.java
@@ -4,39 +4,58 @@ import java.util.ArrayList;
 
 public class GestoreFlotta {
     //attribute
-    private ArrayList<Veicolo> listOfVehicle=new ArrayList<>();
+    private ArrayList<Veicolo> listOfVehicle = new ArrayList<>();
 
-   //constructor
+    //constructor
     public GestoreFlotta(ArrayList<Veicolo> listOfVehicle) {
         this.listOfVehicle = listOfVehicle;
     }
 
     //methods
     public void AddVehicle(Veicolo vehicle) {
-        listOfVehicle.add(vehicle);
-    }
-
-    public int numberOfAuto() {
-        int cont=0;
-        for (Veicolo veicolo: listOfVehicle) {
-         if(veicolo instanceof Auto) {
-             cont++;
-         }
-        }
-        return cont;
-    }
-
-    public int numberOfMoto() {
-        int cont2=0;
-        for (Veicolo veicolo: listOfVehicle) {
-            if(veicolo instanceof Moto) {
-                cont2++;
+        boolean check = true;
+        for (Veicolo veicolo : listOfVehicle) {
+            if (vehicle.getNumberOfPlate() == veicolo.getNumberOfPlate()) {
+                check = false;
             }
         }
-        return cont2;
+        if (check == true) {
+            listOfVehicle.add(vehicle);
+        } else {
+            System.out.println("non puoi inserire il veicolo perche è gia presente");
+        }
     }
 
+        public int numberOfAuto () {
+            int cont = 0;
+            for (Veicolo veicolo : listOfVehicle) {
+                if (veicolo instanceof Auto) {
+                    cont++;
+                }
+            }
+            return cont;
+        }
+
+        public int numberOfMoto () {
+            int cont2 = 0;
+            for (Veicolo veicolo : listOfVehicle) {
+                if (veicolo instanceof Moto) {
+                    cont2++;
+                }
+            }
+            return cont2;
+        }
+
+        public Veicolo foundVehicleThroughNumberOfPlate ( int number){
+            Veicolo vehicle_found = null;
+            for (Veicolo vehicle : listOfVehicle) {
+                if (vehicle.getNumberOfPlate() == number) {
+                    vehicle_found = vehicle;
+                }
+            }
+            return vehicle_found;
+        }
 
 
+    }
 
-}
--- src/org/generation/jaita/veicoli/Veicolo.java
+++ src/org/generation/jaita/veicoli/Veicolo.java
@@ -1,11 +1,15 @@
 package org.generation.jaita.veicoli;
 
 public class Veicolo {
-    private int numeroTarga;
+    private int numberOfPlate;
     private int yearOfImmatricolation;
 
-    public Veicolo(int numeroTarga, int yearOfImmatricolation) {
-        this.numeroTarga = numeroTarga;
+    public Veicolo(int numberOfPlate, int yearOfImmatricolation) {
+        this.numberOfPlate = numberOfPlate;
         this.yearOfImmatricolation = yearOfImmatricolation;
+    }
+
+    public int getNumberOfPlate() {
+        return numberOfPlate;
     }
 }
